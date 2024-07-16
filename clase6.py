d5675c778403f0431da3d9937ddb39cf68d3c4f2
Alvaro <alvaromejia142@gmail.com>
2024-07-16 09:13:09 -0600

    CLASE 6 OPERACIONES MATEMATICAS PARTE 1

---
+++ clase6.py
@@ -0,0 +1,34 @@
+#         "CLASE 6 OPERACIONES MATEMATICAS"
+
+#OPERADORES NUMÉRICOS
+a = 10
+b = 3
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("Potenciación:", a ** b)
+print("División:", a / b)
+print("Parte entera de la División:", a // b)
+print("Módulo:", a % b )
+a = a + 2
+print(a)
+
+
+#PEMDAS:
+
+#Potenciación
+#Exponenciación
+#Matemáticas
+#División
+#Adición
+#Sustracción
+
+operation_1 = 2 + 3 * 4
+operation_2 = 2 + (3 * 4)
+print(operation_1)
+print(operation_2)
+
+operation_3 = (2 + 3) * (4 ** 2) / 8 - 1
+print(operation_3)
+
