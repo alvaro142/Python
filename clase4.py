2642a1f92216afd97479c8e4816afdc3bdf81345
Alvaro <alvaromejia142@gmail.com>
2024-07-16 09:10:14 -0600

    ejemplos del uso de python

---
+++ clase4.py
@@ -0,0 +1,15 @@
+name = 'ALVARO'
+last_name = '   Mejia   Cortez  '
+print(name * 5)
+print(name + ' ' + last_name)
+
+#Nos contabiliza el numero de letras que tiene una cadena
+print(len(last_name))
+print(len(name))
+
+#transformar en minusculas y mayusculas
+print(name.lower())
+print(name.upper())
+
+#quita espacios de una cadena
+print(last_name.strip())
