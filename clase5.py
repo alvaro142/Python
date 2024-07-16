f753d5830640dffa904e184c3f9d2f9fdd936366
Alvaro <alvaromejia142@gmail.com>
2024-07-16 09:12:23 -0600

    CLASE 5 ENTEROS, FLOTANTES Y BOOLEANOS

---
+++ clase5.py
@@ -0,0 +1,27 @@
+#ENTEROS, FLOTANTES Y BOOLEANOS
+
+#ENTEROS
+x = 10
+
+#FLOTANTES
+y = 5.678
+#z = 1.2E6
+#a = 1.2E-6
+
+
+print(type(x))
+print(type(y))
+#print(z)
+#print(a)
+
+#OPERACIONES
+print(x + x)
+print(x + y)
+
+#BOOLEANOS
+is_true = True
+is_false = False
+
+print(is_true)
+print(is_false)
+print(type(is_true))
